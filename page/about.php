d58c46a2ffbbdcecb3663c113233d864b3179ffc
VY LE NGOC THAO <lengocthaovy2435@gmail.com>
2025-11-07 19:54:12 +0700

    Initial setup with CI/CD

---
+++ page/about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>About us</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<style>
+.header{
+    height: 20vh;
+    background-color: aqua;
+}
+</style>
+</head>
+<body>
+    <div class="container">
+        <div class="header"></div>
+        <div class="row">
+            <div class="col-6"></div>
+             <div class="col-6"></div>
+
+        </div>
+    </div>
+</body>
+</html>
